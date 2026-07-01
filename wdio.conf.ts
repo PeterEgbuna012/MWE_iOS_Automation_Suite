34fdee6c512cad01096f6713a5ea5f4474669cf9
PeterEgbuna012 <131350597+PeterEgbuna012@users.noreply.github.com>
2026-07-01 19:25:21 +0100

    Updated

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -23,7 +23,7 @@ export const config: WebdriverIO.Config = {
     // The path of the spec files will be resolved relative from the directory of
     // of the config file unless it's absolute.
     //
-    specs: ['./test/features/MWE_RS_051_Orange_Indicator_Displayed.feature'],
+    specs: ['./test/features/MWE_RS_001_Login_Logout.feature'],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
